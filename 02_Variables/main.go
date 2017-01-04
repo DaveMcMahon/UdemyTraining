b213bda9706e0b3730c2d6d97379a3c79f613810
Dave McMahon <davidmcmhn@gmail.com>
2017-01-04 20:42:54 +0000

    Variables Section Added

---
+++ 02_Variables/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	num1 := 10
+	num2 := 100
+	name := "Dave"
+	decNum := 4.56
+
+	var myNum int = 10
+	var myName string = "Dave"
+
+	fmt.Printf("%T \n", myNum)
+	fmt.Printf("%T \n", myName)
+
+	fmt.Printf("%v \n", num1)
+	fmt.Printf("%v \n", num2)
+	fmt.Printf("%v \n", name)
+	fmt.Printf("%v \n", decNum)
+
+}
