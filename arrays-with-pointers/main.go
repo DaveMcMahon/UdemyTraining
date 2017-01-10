5c324c19ac90ba2773834a15e36d54c648665ad6
Dave McMahon <davidmcmhn@gmail.com>
2017-01-10 17:03:41 +0000

    Arrays with Pointers modified

--- arrays-with-pointers/main.go
+++ arrays-with-pointers/main.go
@@ -13,8 +13,16 @@ func main() {
 	myArray := [...]int{12, 34, 67, 34, 66, 100}
 	baseAll(&myArray)
 
+	var myOtherArray [6]int = [...]int{1, 2, 3, 4, 5, 6}
+	baseAll(&myOtherArray)
+
 	for _, v := range myArray {
 		fmt.Println(v)
 	}
 
+	fmt.Println("********")
+
+	for _, v := range myOtherArray {
+		fmt.Println(v)
+	}
 }
